776ed268ed54dd4bcac02cb51f6d3e6078765c04
Shahbaz Alam <88643897+Alam9835@users.noreply.github.com>
2024-03-04 20:35:37 +0530

    Add files via upload

---
+++ Random Color Generator/js/app.js
@@ -0,0 +1,10 @@
+const setcolor=() =>{
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    color.innerHTML = "#" + randomColor;
+  }
+  
+  
+
+  click.addEventListener("click",setcolor);
+  setcolor();
